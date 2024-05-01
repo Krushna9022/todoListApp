29f6d5fe6708dae439c505a5d96b4b43b4cb0d1c
Krushna9022 <158745029+Krushna9022@users.noreply.github.com>
2024-05-01 20:06:32 +0530

    created the todo app (#1)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+    let [todolist, setTodolist] = useState([])
+
+
+    let saveToDoList = (event) => {
+
+        let toname = event.target.toname.value;
+        if (!todolist.includes(toname)) {
+            let finalTodolist = [...todolist, toname]
+            setTodolist(finalTodolist)
+        } else
+            alert("todo exist alreaddy")
+        event.preventDefault();
+
+    }
+    let list = todolist.map((value, index) => {
+        return (<TodolistItems value={value} key={index} indexNumber={index}
+            todolist={todolist}
+            setTodolist={setTodolist}
+        />
+        )
+    })
+    return (
+        <div className="App">
+            <h1>Todo App</h1>
+            <form onSubmit={saveToDoList}>
+                <input type="text" name='toname' />
+                <button >Add</button>
+            </form>
+            <div className='display'>
+                <ul >
+                    {list}
+                </ul>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
+
+
+function TodolistItems({ value, indexNumber, todolist, setTodolist }) {
+    let deleterow = () => {
+        let finaldata = todolist.filter((v, i) => i !== indexNumber)
+        setTodolist(finaldata)
+
+    }
+    return (
+        <li>{indexNumber + 1} {value} <span onClick={deleterow}>&#x274C;</span></li>
+
+    )
+}
